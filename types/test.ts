34779e6e8bc70e7af1cbdfcf35b6c27052f64187
gpi <gpimpale29@gmail.com>
2021-10-18 10:31:55 -0700

    add types

---
+++ types/test.ts
@@ -0,0 +1,20 @@
+import winkNlp, {
+  Sentences,
+  CustomEntities,
+  SelectedCustomEntities,
+  Entities,
+  SelectedEntities,
+  Tokens,
+  SelectedTokens,
+  ItemCustomEntity,
+  ItemEntity,
+  ItemSentence,
+  ItemToken,
+  Document,
+  ItsFunction,
+  AsFunction,
+} from 'wink-nlp';
+import model from 'wink-eng-lite-web-model';
+
+// $ExpectType Model
+model;
