90693fdaf91d37a6bc67711385f24076736ec78e
Sanjaya Kumar Saxena <sanjaya@graype.in>
2024-05-06 20:05:46 +0530

    refactor(*): update type definitions
    Co-authored-by: Rachna <rachna@graype.in>

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,48 @@
 // Minimum TypeScript Version: 4.0
 
 declare module 'wink-eng-lite-web-model' {
-   // turn off exporting by default since we don't want to expose internal details
-   export {};
+  // turn off exporting by default since we don't want to expose internal details
+  export {};
 
-   import { Model } from 'wink-nlp';
+  // *** BEGIN Language Model Specific Declarations ***
+  // These should be always in sync with the winkNLP's type declarations. 
+  // these types are internal details of the implementing model
+  type StemAddon = unknown;
+  type LemmatizeAddon = unknown;
+  type ReadabilityStatsAddon = unknown;
+  type WordVectorsAddon = unknown;
+
+  // optional addons that some language models may have
+  export interface ModelAddons {
+    stem?: StemAddon;
+    lemmatize?: LemmatizeAddon;
+    readabilityStats?: ReadabilityStatsAddon;
+    wordVectors?: WordVectorsAddon;
+  }
+
+  // these types are internal details of the implementing model
+  type CoreModel = unknown;
+  type SBDModel = unknown;
+  type POSModel = unknown;
+  type NERModel = unknown;
+  type NEGATIONModel = unknown;
+  type SAModel = unknown;
+  type CERMetaModel = unknown;
+  type FeatureFn = unknown;
+
+  // A language model
+  export interface Model {
+    core: CoreModel;
+    sbd: SBDModel;
+    pos: POSModel;
+    ner: NERModel;
+    negation: NEGATIONModel;
+    sa: SAModel;
+    metaCER: CERMetaModel;
+    featureFn: FeatureFn;
+    addons: ModelAddons;
+  }
+  // *** END Language Model Specific Declarations ***
 
    const model: Model;
    export default model;
--- types/test.ts
+++ types/test.ts
@@ -1,19 +1,3 @@
-import winkNlp, {
-  Sentences,
-  CustomEntities,
-  SelectedCustomEntities,
-  Entities,
-  SelectedEntities,
-  Tokens,
-  SelectedTokens,
-  ItemCustomEntity,
-  ItemEntity,
-  ItemSentence,
-  ItemToken,
-  Document,
-  ItsFunction,
-  AsFunction,
-} from 'wink-nlp';
 import model from 'wink-eng-lite-web-model';
 
 // $ExpectType Model
